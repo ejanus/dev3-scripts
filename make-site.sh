9943d1b51a9833c8e37fa34343a3e7828ad654fc
Erek Janus <erek.janus@kavokerrgroup.com>
2017-05-11 12:49:29 -0700

    src-all comment on make-site.sh after run

--- make-site.sh
+++ make-site.sh
@@ -161,7 +161,7 @@ finalize() {
   echo ""
   echo "IMPORTANT: To clear your alias cache please run the following command:"
   echo ""
-  echo "src-aliases && src-bashrc"
+  echo "src-all"
   echo ""
 
   exit 0
